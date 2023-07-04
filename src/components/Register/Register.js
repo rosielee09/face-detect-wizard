5ac565b1a26df86b349db67128e96cbb6af858f7
rosie <rosie.bh.lee@gmail.com>
2023-07-04 13:27:43 -0700

    update server url

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component {
     this.setState({ password: event.target.value });
   };
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://facerecognitionbackend-p9cj.onrender.com/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
